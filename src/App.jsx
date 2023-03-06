d5597f46af98f7cb00a258a8ac152d7b9213fe4c
justorey <11201083@student.itk.ac.id>
2023-03-06 22:08:57 +0800

    menambahkan teks kenapa mas bro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
 export default function App() {
   return (
     <div>
       <p className="text-lg">Gimana ya bang?</p>
+      <p className="text-lg">Kenapa Mas Bro?</p>
     </div>
   );
 }
